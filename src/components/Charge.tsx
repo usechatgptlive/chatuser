b7679c5b9007603c8092e37a9cc61178189d704d
NPC <1042782229@qq.com>
2023-05-16 21:17:58 +0800

    no message

--- src/components/Charge.tsx
+++ src/components/Charge.tsx
@@ -116,14 +116,14 @@ export default (props: Props) => {
             请选择充值金额
           </span>
           <div class="flex space-x-2">
-            <button onClick={() => { getPaycode(1) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-              1元60次
-            </button>
             <button onClick={() => { getPaycode(5) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-              5元320次
+              5元380次
             </button>
             <button onClick={() => { getPaycode(10) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
-              10元650次
+              10元820次
+            </button>
+            <button onClick={() => { getPaycode(20) }} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
+              20元2200次
             </button>
           </div>
         </Show>
